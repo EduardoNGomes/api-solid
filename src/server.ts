777411d7e5d8def4ed09c63170103ad408ea493e
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-07-03 20:19:03 -0300

    configurando variavel de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app } from "./app";
+import {env} from './env'
 
-app.listen({port:3333,host:'0.0.0.0'}).then(() => {
-  console.log('http server running')
+app.listen({port:env.PORT,host:'0.0.0.0'}).then(() => {
+  console.log('http server running on port ' + env.PORT);
 })
