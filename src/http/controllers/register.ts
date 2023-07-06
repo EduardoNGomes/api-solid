f01825a6eff200f79b8749fb7dae8fb70d9f5f48
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-07-05 21:20:04 -0300

    feat: user error handler

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,8 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { registerUseCase } from '@/use-cases/register'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUserRepository } from '@/repositories/prisma/prisma.users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,13 +14,19 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { email, name, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({
+    const uerRepository = new PrismaUserRepository()
+    const registerUseCase = new RegisterUseCase(uerRepository)
+    await registerUseCase.execute({
       email,
       name,
       password,
     })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
