663384340d0a5c567927955cdf932d7ce38b7b63
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-07-10 20:17:37 -0300

    Caso de uso de historico de check-ins

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface FetchUseCheckInsHistoryRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUseCheckInsHistoryResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUseCheckInsHistoryCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUseCheckInsHistoryRequest): Promise<FetchUseCheckInsHistoryResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserID(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
