d554a73dd571cc8030a14cc13fef50e4afb366f4
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-07-10 14:04:25 -0300

    Utilizando Factory Pattern

---
+++ src/use-cases/factories/make-authenticate-use-case.ts
@@ -0,0 +1,8 @@
+import { PrismaUserRepository } from '@/repositories/prisma/prisma.users-repository'
+import { AuthenticateUseCase } from '../authenticate'
+
+export function makeAuthenticateUseCase() {
+  const userRepository = new PrismaUserRepository()
+  const authenticateUseCase = new AuthenticateUseCase(userRepository)
+  return authenticateUseCase
+}
