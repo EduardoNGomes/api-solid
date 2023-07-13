f60f83468e187dbd07323d610113ee4983843bca
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-07-13 15:32:06 -0300

    Test Environment do Prisma

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,37 @@
+import { randomUUID } from 'crypto'
 import { Environment } from 'vitest'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+// postgresql://docker:docker@localhost:5432/apisolid?schema=public
+
+const prisma = new PrismaClient()
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error()
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('executou')
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+    process.env.DATABASE_URL = databaseURL
 
+    execSync('npx prisma migrate deploy')
     return {
       async teardown() {
-        console.log('teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
