0c65ecdbaa0a401ccc0d8fadf15392044ae3c923
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-07-14 13:50:26 -0300

    criacao refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
@@ -9,8 +10,15 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
-
+app.register(cookie)
 app.register(usersRoutes)
 app.register(gymsRoutes)
 app.register(checkInRoutes)
